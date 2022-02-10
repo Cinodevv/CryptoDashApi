80983618b9f0eed0c394df5baf839622215f7e35
Cinodevv <96893418+Cinodevv@users.noreply.github.com>
2022-02-10 09:01:56 -0800

    ApiCrypto Full Upload.

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-key': '6a355ad19bmshd22585a37139343p1bcdffjsn5c69e6014d35',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com'
+};
+const baseUrl='https://bing-news-search1.p.rapidapi.com';
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({baseUrl}),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
